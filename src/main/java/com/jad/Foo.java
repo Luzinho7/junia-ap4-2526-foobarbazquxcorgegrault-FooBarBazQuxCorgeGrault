8a5b837db19cda5edd9af6e0416c78e40f04542a
Lucas <lucas.fernandes3319@gmail.com>
2025-09-18 14:07:18 +0200

    Push du début de la Classe Grault

--- src/main/java/com/jad/Foo.java
+++ src/main/java/com/jad/Foo.java
@@ -1,4 +1,18 @@
 package com.jad;
 
 public class Foo {
+
+}
+    private Bar bar = new Bar();
+
+    private List<Baz> bazs = new ArrayList<>();
+
+    private Qux qux = new Qux();
+
+    private Corge corge;
+
+    private List<Grault> graults = new ArrayList<>();
+
+    public Foo(Bar bar) {
+        this.bar = bar;
 }
