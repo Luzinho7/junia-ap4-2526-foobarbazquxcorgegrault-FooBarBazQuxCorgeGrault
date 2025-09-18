8bc716c5b1fca8db41f2f12277ee082e0a2d7b21
Lucas <lucas.fernandes3319@gmail.com>
2025-09-18 13:39:58 +0200

    Push de la classe Grault

--- src/main/java/com/jad/Grault.java
+++ src/main/java/com/jad/Grault.java
@@ -1,4 +1,14 @@
 package com.jad;
 
 public class Grault {
+    private final Foo foo;
+
+    public Grault(Foo foo) {
+        this.foo = foo;
+    }
+    public Foo getFoo() {
+        return this.foo;
+    }
+
 }
+
