68d99946c518f2bc921ea566b66c7a2c59ffaed3
Lucas <lucas.fernandes3319@gmail.com>
2025-09-18 13:35:44 +0200

    Push de la classe corge

--- src/main/java/com/jad/Corge.java
+++ src/main/java/com/jad/Corge.java
@@ -1,4 +1,19 @@
 package com.jad;
 
 public class Corge {
+
+    private final Foo foo;
+
+    public Foo getFoo() {
+        return this.foo;
+    }
+
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }
+
+    public Corge(Foo foo) {
+        this.foo = foo ;
+    }
 }
+
